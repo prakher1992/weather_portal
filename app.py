1cc5560dbca1b9c1725df2f01586b63cb7a03bea
prakher <prakher1992@gmail.com>
2023-07-17 17:07:31 +0530

    login and registration forms

--- app.py
+++ app.py
@@ -1,13 +1,34 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
-
+app.config['SECRET_KEY'] = '327027639660844979580338508137590406252'
 
 
 @app.route('/')
-def hello_world():
+@app.route('/home')
+def home():
     return render_template("home.html")
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.email.data}!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Register', form=form)
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@weather.com' and form.password.data == 'password':
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Please check username and password', 'danger')
+    return render_template('login.html', title='Login', form=form)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
